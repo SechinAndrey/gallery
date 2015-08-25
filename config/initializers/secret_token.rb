36b586ce1d2e0f3f636b207f6a30bda9bd16213e
SechinAndrey <andreyka12@mail.ru>
2015-08-25 17:38:51 +0300

    Добавление гемов

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gallery::Application.config.secret_key_base = '6576851eafc23f681e37a2bba3a3bde797c747aea82e0ff31ff830152512ae83ae1e001d361521706c2dc3c59b6ccef91c88415d8fdb907c8b62de232069ede6'
