8e577e3b361612e075238296f20f23a96fdc24fb
SechinAndrey <andreyka12@mail.ru>
2015-09-30 17:35:04 +0300

    Локализация на русский и английский. Нормальный pluralize русского языка. v3

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,6 @@ module Gallery
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
      config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
-     config.i18n.default_locale = :ru
+     config.i18n.default_locale = :en
   end
 end
